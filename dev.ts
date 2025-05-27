4d82bb423352bb568067fe643ce5d942e001db02
kivlor <hello@kivlor.com>
2025-05-28 07:07:37 +1000

    omg, move cron to dev since that's the start task

--- dev.ts
+++ dev.ts
@@ -2,7 +2,17 @@
 
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
+import { embedStars } from "./embed.ts";
 
 import "$std/dotenv/load.ts";
 
 await dev(import.meta.url, "./main.ts", config);
+
+if (!Deno.args.includes("build")) {
+  Deno.cron(
+    "0 0 * * *",
+    async () => {
+      await embedStars();
+    },
+  );
+}
--- main.ts
+++ main.ts
@@ -9,15 +9,5 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
-import { embedStars } from "./embed.ts";
 
 await start(manifest, config);
-
-if (!Deno.args.includes("build")) {
-  Deno.cron(
-    "0 0 * * *",
-    async () => {
-      await embedStars();
-    },
-  );
-}

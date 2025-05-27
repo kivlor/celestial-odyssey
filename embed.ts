70963489b95abcbde5818cdd280a106c0de3fd4c
kivlor <hello@kivlor.com>
2025-05-28 07:48:45 +1000

    final try at deploying a cron before I revert to supabase

--- embed.ts
+++ embed.ts
@@ -81,7 +81,7 @@ async function generateEmbedding(
   return embedding;
 }
 
-async function embedStars(limit?: number): Promise<void> {
+export async function embedStars(limit?: number): Promise<void> {
   const octokit = new Octokit({ auth: Deno.env.get("GH_AUTH_TOKEN") });
 
   const openai = new OpenAI({
@@ -153,7 +153,3 @@ async function embedStars(limit?: number): Promise<void> {
 
   return;
 }
-
-Deno.cron("embed", "0 0 * * *", async () => {
-  await embedStars();
-});
--- main.ts
+++ main.ts
@@ -5,9 +5,14 @@
 /// <reference lib="deno.ns" />
 
 import "$std/dotenv/load.ts";
-
 import { start } from "$fresh/server.ts";
+
+import { embedStars } from "./embed.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
+
+Deno.cron("embed", "0 0 * * *", async () => {
+  await embedStars();
+});
 
 await start(manifest, config);
